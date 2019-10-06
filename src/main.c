0693d6de9212e62b3cf94e82e2ab0f067d9d63fb
n-miyamoto <scotsdowneroad@gmail.com>
2019-10-06 14:52:22 +0900

    implement multi frame decoding

--- src/main.c
+++ src/main.c
@@ -8,14 +8,16 @@
   ******************************************************************************
 */
 
-
 #include "stm32f4xx.h"
 #include "stm32f4xx_nucleo.h"
-
 #include <speex/speex.h>
 
-char input_bytes[100];
-short output_frame[100];
+#define FRAME_SIZE 320
+#define BUFFER_SIZE 10000
+
+
+char input_bytes[BUFFER_SIZE];
+short output_frame[FRAME_SIZE];
 
 int main(void)
 {
@@ -24,7 +26,6 @@ int main(void)
 	void *dec_state;
 
 	speex_bits_init(&bits);
-
 	dec_state = speex_decoder_init(&speex_wb_mode);
 
 	int frame_size;
@@ -33,10 +34,28 @@ int main(void)
 	int enh = 0;
 	speex_decoder_ctl(dec_state, SPEEX_SET_ENH, &enh);
 
-	int wbBytes = 100;
-	speex_bits_read_from(&bits, input_bytes, wbBytes);
 
-	speex_decode_int(dec_state, &bits, output_frame);
+
+	char* ptr = input_bytes;
+	while (1)
+	{
+		// decode byte size
+		int wbBytes = *((int*)ptr);
+		if(wbBytes == 0) break;
+
+		ptr+=sizeof(int);
+
+		/*Copy the data into the bit-stream struct*/
+		speex_bits_read_from(&bits, ptr, wbBytes);
+
+		/*Decode the data*/
+		speex_decode_int(dec_state, &bits, output_frame);
+
+		//play output frame
+
+		//next speex frame
+		ptr+=wbBytes;
+	}
 
 	speex_bits_destroy(&bits);
 	speex_decoder_destroy(dec_state);
